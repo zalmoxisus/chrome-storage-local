d2efd184306bc1fe86f8aabc00b52099560d2127
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-08 13:33:17 +0200

    Fallback to `localStorage` if `chrome.storage` isn't available

--- storage.js
+++ storage.js
@@ -1,26 +1,31 @@
-var storage = {};
+var storage;
 
-storage.getItem = function(key, callback) {
-  chrome.storage.local.get(key, function (obj) {
-    if (obj[key]) callback(null, obj[key]);
-    else callback(chrome.runtime.lastError, null);
-  });
-};
+if (typeof chrome !== 'undefined' && chrome.storage && chrome.storage.local) {
+  storage = {};
 
-storage.setItem = function(key, value, callback) {
-  var obj = {};
-  obj[key] = value;
-  chrome.storage.local.set(obj, function() {
-    if (chrome.runtime.lastError) callback(key);
-  });
-};
+  storage.getItem = function (key, callback) {
+    chrome.storage.local.get(key, function (obj) {
+      if (obj[key]) callback(null, obj[key]);
+      else callback(chrome.runtime.lastError, null);
+    });
+  };
 
-storage.removeItem = chrome.storage.local.remove;
+  storage.setItem = function (key, value, callback) {
+    var obj = {};
+    obj[key] = value;
+    chrome.storage.local.set(obj, function () {
+      if (chrome.runtime.lastError) callback(key);
+    });
+  };
 
-storage.getAllKeys = function(callback) {
-  chrome.storage.local.get(null, function(obj) {
-    callback(null, Object.keys(obj));
-  });
-};
+  storage.removeItem = chrome.storage.local.remove;
+
+  storage.getAllKeys = function (callback) {
+    chrome.storage.local.get(null, function (obj) {
+      callback(null, Object.keys(obj));
+    });
+  };
+}
+else storage = localStorage; 
 
 module.exports = storage;
